082969b483e7fa1251ba770f37ccd190015799ce
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 <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>
2022-09-28 10:09:12 -0300

    first commit

---
+++ sudoku.py
@@ -0,0 +1,63 @@
+class NumberIsInRow(Exception):
+    pass
+
+class NumberIsInColumn(Exception):
+    pass
+
+class InvalidSquareException(Exception):
+    pass
+
+class UserHasWon(Exception):
+    pass
+
+
+class Sudoku():
+    def __init__(self):
+        self.rows = 8
+        self.columns = 8
+        self.board = [
+            [1, 0, 0, 5, 0, 0, 0, 0, 3],
+            [0, 3, 7, 0, 0, 1, 0, 0, 4],
+            [0, 0, 0, 0, 0, 2, 0, 0, 0],
+            [0, 0, 0, 0, 9, 4, 0, 0, 0],
+            [0, 0, 4, 0, 0, 0, 0, 3, 2],
+            [0, 0, 0, 3, 2, 5, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 2, 0, 0],
+            [7, 4, 0, 0, 0, 0, 0, 0, 0],
+            [8, 0, 0, 0, 0, 3, 1, 0, 0],
+        ]
+
+    def set_number(self, number, row, column):
+        # iterate over board lists
+        self.check_square(number, row, column)
+        self.check_column(number, column)
+        self.check_row(number, row)
+        self.board[row][column] = number
+        self.check_if_user_won()
+
+    def check_if_user_won(self):
+        if not any(0 in x for x in self.board): # check every item != 0
+            raise UserHasWon()
+    def check_square(self, number, row, column):
+        def find_nearest_subSquare(row1, nearestRow=0):
+            # recursively find the nearest row or column from left to right
+            if row1 < nearestRow + 3:
+                return nearestRow
+            return find_nearest_subSquare(row1, nearestRow+3)
+        column = find_nearest_subSquare(column)
+        row = find_nearest_subSquare(row)
+        for iterable_row in range(row, row+3):  # each subsquare has 3 elements
+            for iterable_column in range(column, column+3):
+                if number == self.board[iterable_row][iterable_column]:
+                    raise InvalidSquareException()
+
+    def check_row(self, number, row=0):
+        if number in self.board[row]:
+            raise NumberIsInRow()
+
+    def check_column(self, number, column, row=0):  # Recursively check columns
+        if number == self.board[row][column]:
+            raise NumberIsInColumn
+        if (row == len(self.board) -1):  # check the last element in column
+            return None
+        return self.check_column(number, column, row + 1)
---
+++ sudoku_UI.py
@@ -0,0 +1,114 @@
+import os
+import time
+from sudoku import *
+
+# IMPORTED FROM https://github.com/jeffsieu/py-sudoku
+
+def format_board_ascii(board):
+    table = ''
+    size = 9
+    width = 3
+    height = 3
+    cell_length = len(str(size))
+    format_int = '{0:0' + str(cell_length) + 'd}'
+    for i, row in enumerate(board):
+        if i == 0:
+            table += ('+-' + '-' * (cell_length + 1)
+                      * width) * height + '+' + '\n'
+        table += (('| ' + '{} ' * width) * height + '|').format(*
+                                                                [format_int.format(x) if x != None and x != 0 else ' ' * cell_length for x in row]) + '\n'
+        if i == size - 1 or i % height == height - 1:
+            table += ('+-' + '-' * (cell_length + 1)
+                      * width) * height + '+' + '\n'
+
+    return table
+# END
+
+
+def mainMenu():
+    sudoku = Sudoku()
+    while True:
+        line = format_board_ascii(sudoku.board).splitlines()
+        for i in range(len(line)):
+            print('                       ' + line[i])
+        play(sudoku)
+
+
+def play(sudoku):
+    errorColor = '\033[95m'
+    correctColor = '\033[92m'
+    colorEnd = '\033[0m'
+    # print ascii sudoku table
+
+    # check input
+    try:
+        row = int(input(correctColor + '\n\nSelect a Row (0-8) → ' + colorEnd))
+        column = int(
+            input(correctColor + 'Select a Column (0-8) ↑ ' + colorEnd))
+        number = int(
+            input(correctColor + 'Input a number (1-9) ↪ ' + colorEnd))
+        assert (0 < number < 10 and 0 <= row < 10 and 0 <= column < 10)
+        sudoku.set_number(number, row, column)
+    except UserHasWon:
+        print(correctColor + 'You WON ' + colorEnd + '\n')
+    except KeyboardInterrupt:
+        os.system("clear")
+        print(errorColor + """
+              
+               ...
+             ;::::;
+           ;::::; :;
+         ;:::::'   :;
+        ;:::::;     ;.
+       ,:::::'       ;           OOO\
+       ::::::;       ;          OOOOO\
+       ;:::::;       ;         OOOOOOOO
+      ,;::::::;     ;'         / OOOOOOO
+    ;:::::::::`. ,,,;.        /  / DOOOOOO
+  .';:::::::::::::::::;,     /  /     DOOOO
+ ,::::::;::::::;;;;::::;,   /  /        DOOO
+;`::::::`'::::::;;;::::: ,#/  /          DOOO
+:`:::::::`;::::::;;::: ;::#  /            DOOO
+::`:::::::`;:::::::: ;::::# /              DOO
+`:`:::::::`;:::::: ;::::::#/               DOO
+ :::`:::::::`;; ;:::::::::##                OO
+ ::::`:::::::`;::::::::;:::#                OO
+ `:::::`::::::::::::;'`:;::#                O
+  `:::::`::::::::;' /  / `:#
+   ::::::`:::::;'  /  /   `#
+              """+ colorEnd)
+        print(errorColor + "Aun no has terminado" + colorEnd)
+    except NumberIsInRow:
+        print(errorColor + 'The specified number is already in the row' + colorEnd + '\n')
+    except NumberIsInColumn:
+        print(
+            errorColor + 'The specified number is already in the column' + colorEnd + '\n')
+    except InvalidSquareException:
+        print(
+            errorColor + 'The specified number is already in the square' + colorEnd + '\n')
+    except ValueError:
+        print(errorColor + ' Wrong input. Try again.' + colorEnd + '\n')
+    except AssertionError:
+        print(
+            errorColor + 'Wrong number of row / column. Try again.' + colorEnd + '\n')
+    else:
+        print(correctColor + 'Well Done' + colorEnd)
+    
+
+
+if __name__ == "__main__":
+    os.system("reset")
+    print("""
+    \033[92m
+              ██████  █    ██ ▓█████▄  ▒█████   ██ ▄█▀ █    ██ 
+            ▒██    ▒  ██  ▓██▒▒██▀ ██▌▒██▒  ██▒ ██▄█▒  ██  ▓██▒
+            ░ ▓██▄   ▓██  ▒██░░██   █▌▒██░  ██▒▓███▄░ ▓██  ▒██░
+              ▒   ██▒▓▓█  ░██░░▓█▄   ▌▒██   ██░▓██ █▄ ▓▓█  ░██░
+            ▒██████▒▒▒▒█████▓ ░▒████▓ ░ ████▓▒░▒██▒ █▄▒▒█████▓ 
+            ▒ ▒▓▒ ▒ ░░▒▓▒ ▒ ▒  ▒▒▓  ▒ ░ ▒░▒░▒░ ▒ ▒▒ ▓▒░▒▓▒ ▒ ▒ 
+            ░ ░▒  ░ ░░░▒░ ░ ░  ░ ▒  ▒   ░ ▒ ▒░ ░ ░▒ ▒░░░▒░ ░ ░ 
+            ░  ░  ░   ░░░ ░ ░  ░ ░  ░ ░ ░ ░ ▒  ░ ░░ ░  ░░░ ░ ░ 
+                  ░     ░        ░        ░ ░  ░  ░      ░     
+                               ░                          
+    \033[0m  """)
+    mainMenu()
